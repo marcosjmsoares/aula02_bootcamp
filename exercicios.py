593d4a35448e345c30a81cceabadc5a492f4e4e7
marcossoares-dev <55550244+marcossoares-dev@users.noreply.github.com>
2025-03-05 00:22:41 -0300

    exercicio numero 2

--- exercicios.py
+++ exercicios.py
@@ -3,13 +3,16 @@ import math
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-numero_01 = int(input("Insira um numero:"))
-numero_02 = int(input("Insira outro numero:"))
-resultado = numero_01 + numero_02
-print(resultado)
-
+# numero_01 = int(input("Insira um numero:"))
+# numero_02 = int(input("Insira outro numero:"))
+# resultado = numero_01 + numero_02
+# print(resultado)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# numero_01 = int(input("Insira um numero:"))
+# resultado = numero_01 % 5
+# print(resultado)
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # try:
