14dc5f9c9acd3331abb6d88f63da8c879028c528
marcossoares-dev <55550244+marcossoares-dev@users.noreply.github.com>
2025-03-05 00:20:30 -0300

    exercicio 01

--- exercicios.py
+++ exercicios.py
@@ -3,16 +3,22 @@ import math
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero_01 = int(input("Insira um numero:"))
+numero_02 = int(input("Insira outro numero:"))
+resultado = numero_01 + numero_02
+print(resultado)
+
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-try:
-    numero_01 = int(input("Inserir um numero inteiro:"))
-    numero_02 = int(input("Inserir outro numero inteiro:"))
-    resultado = numero_01//numero_02
-    print(resultado)
-except:
-    print("voce nao pode inserir 0 nos campos")
+# try:
+#     numero_01 = int(input("Inserir um numero inteiro:"))
+#     numero_02 = int(input("Inserir outro numero inteiro:"))
+#     resultado = numero_01//numero_02
+#     print(resultado)
+# except:
+#     print("voce nao pode inserir 0 nos campos")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
